26343afc396ad3e55239b6baa40b7060152e5cf6
Titus Jungao <johntitusjungao@gmail.com>
2022-03-29 12:19:06 +0800

    Preprocess invalid data points

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,17 @@
+import numpy as np
 import pandas as pd
 
+
+def format_invalid_data(df):
+    cols = list(df.columns[7:])
+
+    def mapToNan(n):
+        if not str(n).isnumeric():
+            return np.NaN
+    for col in cols:
+        df[col] = df[col].map(mapToNan)
+
+    return df
 
 def format_date(df):
     df['date'] = pd.to_datetime(df['date'])
