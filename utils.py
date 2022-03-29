8568aa2a7cbed28331dc837c7a23a52a342a4f4a
Titus Jungao <johntitusjungao@gmail.com>
2022-03-29 10:50:55 +0800

    Update upload file function

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
-import streamlit as st
 import pandas as pd
-import plotly.express as px
 
 
 # upload csv file
 def upload_file(type="csv"):
     data_file = st.file_uploader("Upload csv file", type=["csv"])
     if data_file is not None:
-        return pd.read_csv(data_file)
+        excel_file = pd.ExcelFile(data_file)
+        df_mt = pd.read_excel(excel_file, sheet_name="MT OSA")
+        df_rmt = pd.read_excel(excel_file, sheet_name="RMT OSA")
+
+        return df_mt, df_rmt
