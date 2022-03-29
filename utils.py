3beb222920e7f6fb40695e3b88c20b23970b22ee
Titus Jungao <johntitusjungao@gmail.com>
2022-03-29 11:06:45 +0800

    Modify upload file for excel files

--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
+import streamlit as st
 import pandas as pd
 
 
 # upload csv file
-def upload_file(type="csv"):
-    data_file = st.file_uploader("Upload csv file", type=["csv"])
+def upload_file(file_type="xlsx"):
+    data_file = st.file_uploader("Upload csv file", type=file_type)
     if data_file is not None:
-        excel_file = pd.ExcelFile(data_file)
-        df_mt = pd.read_excel(excel_file, sheet_name="MT OSA")
-        df_rmt = pd.read_excel(excel_file, sheet_name="RMT OSA")
+        if data_file=="xlsx":
+            excel_file = pd.ExcelFile(data_file)
+            df_mt = pd.read_excel(excel_file, sheet_name="MT OSA")
+            df_rmt = pd.read_excel(excel_file, sheet_name="RMT OSA")
 
         return df_mt, df_rmt
