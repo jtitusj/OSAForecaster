f47efb2c658c85b5d45f9c35a9e61eaa01899ca5
Titus Jungao <johntitusjungao@gmail.com>
2022-03-29 16:21:55 +0800

    Plot Availability

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import numpy as np
+import plotly.graph_objects as go
+
+from plotly.subplots import make_subplots
 
 from utils import upload_file, load_file
 from sidebar import select_data
@@ -19,14 +22,36 @@ st.title("On-Shelf Availability Forecaster")
 if dataframes is not None:
 
     with st.sidebar.expander("Filter", expanded=True):
-        df, out, cols, products = select_data(dataframes)
+        df, out, cols = select_data(dataframes)
 
     filters = np.all([df[col]==out[col].values[0] for col in cols], axis=0)
     outlet_data = df[filters].sort_values(by=['YEAR', 'WEEK'])
 
+    time = outlet_data["YEAR"].astype(str) + ', Week ' + outlet_data["WEEK"].astype(str)
+
     st.header("Data Overview")
     with st.expander("Show Dataset", expanded=True):
-        st.write(outlet_data.loc[:, ['YEAR', 'MONTH', 'WEEK', 'OUTLET'] + products])
+        # st.write(outlet_data.loc[:, ['YEAR', 'MONTH', 'WEEK', 'OUTLET'] + products])
+        st.write(outlet_data)
 
+    st.header("OSA Plots")
+    products = st.multiselect("Select Product(s)", options=list(df.columns[7:]),
+                                                   default=list(df.columns[7:]))
+    
+    fig = make_subplots(rows=len(products), cols=1, shared_xaxes=True,
+                                                    vertical_spacing=0.025)
+    for i in range(len(products)):
+        obj = go.Scatter(
+            x = time,
+            y = outlet_data[products[i]],
+            mode = 'markers',
+            name = products[i]
+        )
+
+        fig.add_trace(obj, row=i+1, col=1)
+    
+    fig.update_layout(height=200*len(products), yaxis_range=[-.05, 7.5])
+
+    st.plotly_chart(fig, use_container_width=True)
 else:
     st.write("Please upload a dataset in the left sidebar.")
