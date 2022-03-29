f468fd957f1ffe20041ac98b17adc463623daa9d
Titus Jungao <johntitusjungao@gmail.com>
2022-03-29 11:21:04 +0800

    Add function for determining outlets

--- utils.py
+++ utils.py
@@ -6,9 +6,12 @@ import pandas as pd
 def upload_file(file_type="xlsx"):
     data_file = st.file_uploader("Upload csv file", type=file_type)
     if data_file is not None:
-        if data_file=="xlsx":
+        if file_type=="xlsx":
             excel_file = pd.ExcelFile(data_file)
             df_mt = pd.read_excel(excel_file, sheet_name="MT OSA")
             df_rmt = pd.read_excel(excel_file, sheet_name="RMT OSA")
 
         return df_mt, df_rmt
+
+def get_outlet_data(df):
+    return df.groupby(list(df.columns[3:7])).count().reset_index().iloc[:, :4]    
