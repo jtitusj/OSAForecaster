afe284a93d2d9fa5deb3769cd89a3a9074035cee
Titus Jungao <johntitusjungao@gmail.com>
2022-03-29 15:10:25 +0800

    create unifying function for preprocesses

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,13 @@ import numpy as np
 import pandas as pd
 
 
+def preprocess(df):
+    df = format_column_names(df)
+    df['WEEK'] = format_week(df['WEEK'])
+    df = format_invalid_data(df)
+
+    return df
+
 def format_column_names(df):
     cols = ["YEAR", "MONTH", "WEEK", "AREA", "GROUP", "ACCOUNT", "OUTLET"]
     cols_df = list(df.columns[:len(cols)])
@@ -11,19 +18,13 @@ def format_week(week):
     return week.map(lambda x: int(str(x).split(" ")[-1]))
 
 def format_invalid_data(df):
-    cols = list(df.columns[7:])
-
-    def mapToNan(n):
+    def map_to_numeric(n):
         if str(n).isnumeric():
-            return n
+            return int(n)
         else:
             return np.NaN
-
-    for col in cols:
-        df[col] = df[col].map(mapToNan)
-
-    return df
-
-def format_date(df):
-    df['date'] = pd.to_datetime(df['date'])
+        
+    for col in df.columns[7:]:
+        df[col] = df[col].map(map_to_numeric)
+    
     return df
