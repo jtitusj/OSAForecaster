b0b5ae4664e02c4a0cf4bc2bb4b1ac559120c3f1
Titus Jungao <johntitusjungao@gmail.com>
2022-03-30 10:46:09 +0800

    Add functions for preprocessing offtake data

--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,9 @@ import pandas as pd
 
 def preprocess(df):
     df = format_column_names(df)
-    df['WEEK'] = format_week(df['WEEK'])
+    df = remove_total_row(df)
+    df['YEAR'] = format_year_column(df['YEAR'])
+    df['WEEK'] = format_week_column(df['WEEK'])
     df = format_invalid_data(df)
 
     return df
@@ -14,7 +16,14 @@ def format_column_names(df):
     cols_df = list(df.columns[:len(cols)])
     return df.rename(columns=dict(zip(cols_df, cols)))
 
-def format_week(week):
+def remove_total_row(df):
+    "Remove row if it contains sums of previous value"
+    return df[df["OUTLET"] != "TOTAL"]
+
+def format_year_column(year):
+    return df["YEAR"].astype(int)
+
+def format_week_column(week):
     return week.map(lambda x: int(str(x).split(" ")[-1]))
 
 def format_invalid_data(df):
