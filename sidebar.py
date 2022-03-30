de00af0f52c9a242ca98355a49d611284e283e16
Titus Jungao <johntitusjungao@gmail.com>
2022-03-30 11:29:12 +0800

    Minor code changes

--- sidebar.py
+++ sidebar.py
@@ -1,33 +1,39 @@
 import streamlit as st
+import numpy as np
 
 from utils import get_outlet_data
 
-def select_data(dfs):
+def select_data(osa_data, offtake_data):
     dataset = st.selectbox("Select Source", options=['MT', "RMT"])  
 
     if dataset == "MT":
-        df = dfs[0]
+        df_osa = osa_data[0]
+        df_offtake = offtake_data[0]
     else: 
-        df = dfs[1] 
+        df_osa = osa_data[1]
+        df_offtake = offtake_data[1]
 
     # outlet data
-    outlet = get_outlet_data(df)
+    outlet = get_outlet_data(df_osa)
     out = outlet.copy()
-    col = out.columns
+    cols = out.columns
 
     area = st.selectbox("Select Area", options=out.iloc[:, 0].unique())
-    out = out[out[col[0]]==area]
+    out = out[out[cols[0]]==area]
 
     group = st.selectbox("Select Group", options=out.iloc[:, 1].unique())
-    out = out[out[col[1]]==group]
+    out = out[out[cols[1]]==group]
 
     account = st.selectbox("Select Account", options=out.iloc[:, 2].unique())
-    out = out[out[col[2]]==account]
+    out = out[out[cols[2]]==account]
 
     outlet_ = st.selectbox("Select Outlet", options=out.iloc[:, 3].unique())
-    out = out[out[col[3]]==outlet_]
+    out = out[out[cols[3]]==outlet_]
 
     # products = st.multiselect("Select Products", options=list(df.columns[7:]),
     #                                              default=list(df.columns[7:]))
 
-    return df, out, col #, products
+    return df_osa, df_offtake, out, cols #, products
+
+def use_select_filter(df, out, cols):
+    return np.all([df[col]==out[col].values[0] for col in cols], axis=0)
